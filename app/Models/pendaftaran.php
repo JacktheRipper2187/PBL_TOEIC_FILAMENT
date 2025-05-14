88019c0e72a083508bdf0df9ad4bc9f323453ff4
SyaqiraNazaret <snazaretna@gmail.com>
2025-05-14 15:01:12 +0700

    Pendaftaran

--- app/Models/pendaftaran.php
+++ app/Models/pendaftaran.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
+$tableName = 'pendaftaran';
+
 class pendaftaran extends Model
 {
     use HasFactory;
