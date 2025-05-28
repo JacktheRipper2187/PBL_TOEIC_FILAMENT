e6fccd8aadacea9875429aa522617d349e73fc98
Devita Dwi Lestari <devitadwilestari2@gmail.com>
2025-05-28 07:31:43 +0700

    tombol langsung ke panel pendaftaran

--- resources/views/Depan.blade.php
+++ resources/views/Depan.blade.php
@@ -171,42 +171,34 @@
         </div>
     </section>
 
-    <!-- Pendaftaran Section-->
-    <section class="page-section portfolio" id="pendaftaran">
-        <div class="container">
-            <!-- Pendaftaran Section Heading-->
-            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-4">{{ __('messages.pendaftar') }}</h2>
-            <!-- Icon Divider-->
-            <div class="divider-custom">
-                <div class="divider-custom-line"></div>
-                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                <div class="divider-custom-line"></div>
-            </div>
-            <!-- Pendaftaran Grid Items-->
-            <div class="row justify-content-center">
-                @php
-                    $i=1;
-                @endphp
-                @foreach ($pendaftaran as $item)
-            {{-- <!-- Pendaftaran Item {{ $i }}--> --}}
-                <div class="col-md-6 col-lg-4 mb-5">
-                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal{{$i}}">
-                        <div
-                            class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                            <div class="portfolio-item-caption-content text-center text-white"><i
-                                    class="fas fa-plus fa-3x"></i></div>
-                        </div>
-                         <img class="img-fluid" src="{{ asset('uploads/' . $item->thumbnail) }}" alt="..." />
-                    </div>
-                </div>
-                {{-- <!-- last Pendaftaran {{$i}}--> --}}
-               @php
-                   $i++;
-               @endphp
-                @endforeach 
-            </div>
+   <!-- Pendaftaran Section-->
+<section class="page-section portfolio" id="pendaftaran">
+    <div class="container">
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-4">{{ __('messages.pendaftar') }}</h2>
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
         </div>
-    </section>
+        <div class="row justify-content-center">
+            @foreach ($pendaftaran as $item)
+            <div class="col-md-6 col-lg-4 mb-5">
+                <a href="{{ $item->link ? $item->link : route('formpendaftaran') }}" 
+                   class="portfolio-item mx-auto d-block" 
+                   target="{{ $item->link ? '_blank' : '_self' }}">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white">
+                            <i class="fas fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img class="img-fluid" src="{{ asset('uploads/' . $item->thumbnail) }}" alt="..." />
+                </a>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
+
 
 <!-- Jadwal Section -->
 <section class="page-section bg-primary text-white mb-0" id="jadwal">
